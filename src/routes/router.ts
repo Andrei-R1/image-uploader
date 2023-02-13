3cf925f4cb35869bfc01075bf6597eaa7d199a31
Andrei Rivera <andreirive2003@gmail.com>
2023-02-13 17:23:47 -0600

    Initial setup

---
+++ src/routes/router.ts
@@ -0,0 +1,11 @@
+import { Application } from "express";
+import imagesRouter from "./images";
+import authRouter from "./auth";
+
+export default function router(app: Application): void {
+  /**
+   * Every source are specifed here
+   */
+  app.use("/images", imagesRouter);
+  app.use("/auth", authRouter);
+}
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Application, json } from "express";
+import morgan from "morgan";
+import * as http from "http";
+
+const app = express();
+app.use(morgan("dev"));
+app.use(json());
+
+export default class Server {
+  //Load router
+  router(routes: (app: Application) => void) {
+    routes(app);
+    return this;
+  }
+  //Listen server
+  listen(port: number, hostname: string): Application {
+    http.createServer(app).listen(port, hostname, () => {
+      console.log(`⭐Server running and listen on http://${hostname}:${port} `);
+    });
+    return app;
+  }
+}
