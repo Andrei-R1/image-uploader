5f4243a18092f44defa872cbe2cb22b51d1f9f47
Andrei Rivera <andreirive2003@gmail.com>
2023-02-14 15:26:08 -0600

    Image controlers

--- src/controllers/images.controller.ts
+++ src/controllers/images.controller.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
+import { UploadImageSchema } from "../models/image.models";
+import imageService from "../services/image.service";
+import { BaseController } from "../types/base.controller";
 
-class ImagesController {
-  getImage(req: Request, res: Response) {
-    //to be defined
-    res.send("hello");
+class ImagesController extends BaseController{
+  async getImage(req: Request, res: Response) {
+    try{
+      const result = await imageService.findImageById(Number(req.params.id));
+        this.responseHandler(res, result, 200);
+    }catch(error: any){
+      if (error.code && error.code === "P2002") {
+        this.responseHandler(res, { error: "Image not found" }, 404);
+      }
+      this.errorHandler(res, error);
+    }
   }
-  uploadImage(req: Request, res: Response) {
-    //to be defined
-    res.send("hello");
+
+  async uploadImage(req: Request, res: Response) {
+    try {
+      const data = await UploadImageSchema.validateAsync(req.body);
+      const result = await imageService.createImage(data.url);
+      this.responseHandler(res, result, 200);
+    } catch (error: any) {
+      if (error.code && error.code === "P2002") {
+        this.responseHandler(res, { error: "Image was already uploaded" }, 400);
+      } else {
+        this.errorHandler(res, error);
+      }
+    }
   }
 }
 
